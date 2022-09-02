9dcc2094b16f0a081a4eface2683365805fb7e02
sigristarisa <arisasigrist.ch@gmail.com>
2022-09-02 15:20:24 +0200

    setup prismic

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { PrismicProvider } from "@prismicio/react";
+import { client } from "./prismic";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -10,7 +12,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <BrowserRouter>
     <React.StrictMode>
-      <App />
+      <PrismicProvider client={client}>
+        <App />
+      </PrismicProvider>
     </React.StrictMode>
   </BrowserRouter>
 );

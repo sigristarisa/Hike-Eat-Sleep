1f5bbe3f44580f71d4cfcf0c9c2df4d437eea91e
sigristarisa <arisasigrist.ch@gmail.com>
2022-09-19 11:09:09 +0900

    created HeaderMobile

--- src/Components/Header/HeaderMobile.tsx
+++ src/Components/Header/HeaderMobile.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import "./HeaderMobile.css";
 
 const HeaderMobile: React.FC = () => {
   return (
-    <header>
+    <header className='header-mobile_container grid-columns_two align-items_center'>
       <h1>wanderfux</h1>
-      <button>navi</button>
+      <button className='justify-items_end'>navi</button>
     </header>
   );
 };

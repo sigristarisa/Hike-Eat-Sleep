308ecf0c03fb4fc8c6c3de53ff1815b2872e4f90
sigristarisa <arisasigrist.ch@gmail.com>
2022-09-20 15:26:28 +0900

    styled for Blog Page mobile

--- src/Components/Blog/Desktop/BlogListHeader.tsx
+++ src/Components/Blog/Desktop/BlogListHeader.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-const BlogListHeader: React.FC = () => {
+interface props {
+  isMobile: Function;
+}
+
+const BlogListHeader: React.FC<props> = ({ isMobile }) => {
   const navigate = useNavigate();
   return (
-    <div className='blog-list-header_container grid-columns_two-extend-auto align-items_center'>
-      <p>outdoor blog by wanderfux</p>
+    <div
+      className={`blog-list-header_container${isMobile()} grid-columns_two-extend-auto align-items_center`}
+    >
+      <p>outdoor blog{isMobile() && <br></br>} by wanderfux</p>
       <button onClick={() => navigate("../subscribe")}>SUBSCRIBE</button>
     </div>
   );
--- src/Components/Blog/Desktop/BlogPage.tsx
+++ src/Components/Blog/Desktop/BlogPage.tsx
@@ -5,6 +5,7 @@ import Footer from "../../Footer/Desktop/Footer";
 import { WindowSize } from "../../../helpers/types";
 import hikeEatSleepLogo from "../../../assets/HikeEatSleep_Logo.png";
 import hikeEatSleepBlog from "../../../assets/HikeEatSleep_Blog.png";
+import hikeEatSleepBlogMobile from "../../../assets/HikeEatSleep_BlogMobile.png";
 import "./BlogPage.css";
 import "../Mobile/BlogPageMobile.css";
 
@@ -20,15 +21,19 @@ const BlogPage: React.FC<props> = ({ windowSize }) => {
   return (
     <div className={`blog-page_container${isMobile()} place-items_center`}>
       <section className={`blog-list_container${isMobile()}`}>
-        <BlogListHeader />
+        <BlogListHeader isMobile={isMobile} />
         <BlogList />
       </section>
-      <div className='blog-logo-img_wrapper'>
+      <div className={`blog-logo-img_wrapper${isMobile()}`}>
         <div className={`blog-logo_container${isMobile()}`}>
           <img src={hikeEatSleepLogo} alt='hike eat sleep logo' />
         </div>
-        <div className='blog-img_container'>
-          <img src={hikeEatSleepBlog} alt='hike eat sleep logo' />
+        <div className={`blog-img_container${isMobile()} place-items_center`}>
+          {windowSize.innerWidth > 600 ? (
+            <img src={hikeEatSleepBlog} alt='hike eat sleep logo' />
+          ) : (
+            <img src={hikeEatSleepBlogMobile} alt='hike eat sleep logo' />
+          )}
         </div>
       </div>
       <Footer />

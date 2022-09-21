561630370a557291b33d64c158fcf63d3938e0af
sigristarisa <arisasigrist.ch@gmail.com>
2022-09-21 16:38:58 +0900

    added pop up animation for the nav page

--- src/Components/Header/Mobile/HeaderMobile.tsx
+++ src/Components/Header/Mobile/HeaderMobile.tsx
@@ -10,24 +10,21 @@ interface props {
 const HeaderMobile: React.FC<props> = ({ isNavPage, setIsNavPage }) => {
   const navigate = useNavigate();
 
-  const closeNavPage = (): void => setIsNavPage(false);
-  const openNavPage = (): void => setIsNavPage(true);
+  const closeNavPage = (): void => {
+    setIsNavPage(false);
+  };
+
+  const openNavPage = (): void => {
+    setIsNavPage(true);
+  };
 
   const handleWanderfuxBtn = (): void => {
     navigate("../");
     setIsNavPage(false);
   };
 
-  const setBackgroundColor = (): string => {
-    return isNavPage
-      ? "header-mobile_container red-background"
-      : "header-mobile_container";
-  };
-
   return (
-    <header
-      className={`${setBackgroundColor()} grid-columns_two align-items_center`}
-    >
+    <header className='header-mobile_container grid-columns_two align-items_center'>
       <h1 onClick={() => handleWanderfuxBtn()}>wanderfux</h1>
       {isNavPage ? (
         <button className='justify-items_end' onClick={() => closeNavPage()}>
--- src/Components/Header/Mobile/NavPage.tsx
+++ src/Components/Header/Mobile/NavPage.tsx
@@ -13,21 +13,24 @@ const NavPage: React.FC<props> = ({ setIsNavPage }) => {
     { id: 1, name: "blog", navigate: "../blog" },
     { id: 2, name: "shop", navigate: "../shop" },
   ];
+
   return (
-    <main className='nav-page_container place-items_center'>
-      <nav>
-        <ul>
-          {navButtonArr.map((navButton, index) => (
-            <NavPageNavItem
-              key={index}
-              navButton={navButton}
-              index={index}
-              setIsNavPage={setIsNavPage}
-            />
-          ))}
-        </ul>
-      </nav>
-    </main>
+    <div className='hi'>
+      <main className='nav-page_container place-items_center'>
+        <nav>
+          <ul>
+            {navButtonArr.map((navButton, index) => (
+              <NavPageNavItem
+                key={index}
+                navButton={navButton}
+                index={index}
+                setIsNavPage={setIsNavPage}
+              />
+            ))}
+          </ul>
+        </nav>
+      </main>
+    </div>
   );
 };
 

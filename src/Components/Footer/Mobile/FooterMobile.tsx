4ab0060eba3007d08824eb961ef1cbe92d661fb0
sigristarisa <arisasigrist.ch@gmail.com>
2022-09-26 14:10:55 +0900

    adjusted footer mobile

--- src/Components/Footer/Mobile/FooterMobile.tsx
+++ src/Components/Footer/Mobile/FooterMobile.tsx
@@ -11,19 +11,19 @@ const FooterMobile: React.FC = () => {
           ©2020 ・ wanderfux ・<br></br> switzerland ・ Impressum
         </p>
       </div>
-      <div className='footer-social-media-mobile_wrapper grid-columns_two align-items_center'>
+      <div className='footer-social-media-mobile_wrapper grid-columns_two place-items_center'>
         <div className='footer-social-media-mobile_container grid-colums_two-extend-two align-items_center'>
-          <div className='footer-social-media-icon-mobile_container justify-items_start'>
+          <div className='footer-social-media-icon-mobile_container instagram  place-items_center'>
             <img src={iconInstagram} alt='instagram' />
           </div>
           <a href='https://www.instagram.com/by.wanderfux'>@by.wanderfux</a>
         </div>
         <div className='footer-social-media-mobile_container grid-colums_two-extend-two align-items_center'>
-          <div className='footer-social-media-icon-mobile_container justify-items_start'>
+          <div className='footer-social-media-icon-mobile_container youtube place-items_center'>
             <img src={iconYoutube} alt='youtube' />
           </div>
           <a
-            className='place-items_center'
+            className='align-items_center'
             href='https://www.youtube.com/channel/UCboUr2INTVh84ba0yoElbyw/featured'
           >
             @wanderfux
